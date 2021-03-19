fdc59e4999e5179dd2aa398ae4018eebf817c087
Daniel Gill <56319752+DanGill@users.noreply.github.com>
2021-03-19 21:48:07 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cli-essentials",
-    version="0.0.1",
+    version="0.0.2",
     author="Daniel Gill",
     description="Python CLI building essentials",
     long_description=long_description,
